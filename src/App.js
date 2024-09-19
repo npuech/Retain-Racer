1190efc9191969b1340df59f71a46d7e55b66259
Noa Puech <noapuech05@gmail.com>
2024-09-19 22:36:50 +0200

    Reprise sur le projet Retain Racer + Modification App

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from "react";
 import "./index.css";
 import { HashRouter as Routes, Route } from "react-router-dom"
-import { NavBar } from "./composents/NavBar";
-import { Accueil } from "./pages/Accueil";
+import NavBar from "./composents/NavBar";
+import  Accueil from "./pages/Accueil";
 import { Contact } from "./pages/contact";
 import { Jeux } from "./pages/Jeux";
 import { QuizzLuxe } from "./pages/QuizzLuxe";
 import { Mentions } from "./pages/MentionsLegales";
-import { Footer } from "./composents/Footer";
+import Footer from "./composents/Footer";
 
 function App() {
 
   return (
     <div>
-
+      <NavBar />
+      <Accueil />
+      <Footer />
     </div>
   );
 
