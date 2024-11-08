747f4df087f522eb3830f0105f5e472ccf28894b
Noa Puech <noapuech05@gmail.com>
2024-11-08 14:22:28 +0100

    ok

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import "./index.css";
 import { HashRouter as Routes, Route } from "react-router-dom"
 import NavBar from "./composents/NavBar";
 import  Accueil from "./pages/Accueil";
-import { Contact } from "./pages/contact";
-import { Jeux } from "./pages/Jeux";
-import { QuizzLuxe } from "./pages/QuizzLuxe";
-import { Mentions } from "./pages/MentionsLegales";
+import  Contact  from "./pages/contact";
+import Jeux from "./pages/Jeux";
+import QuizzLuxe from "./pages/QuizzLuxe";
+import Mentions from "./pages/MentionsLegales";
 import Footer from "./composents/Footer";
 
 function App() {
